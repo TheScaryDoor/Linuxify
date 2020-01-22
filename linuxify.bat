d20101dfa2423aa03ba4218d830f7f731a73efb4
TheScaryDoor <42273768+TheScaryDoor@users.noreply.github.com>
2020-01-21 19:13:35 -0500

    Add files via upload

---
+++ linuxify.bat
@@ -0,0 +1,4 @@
+echo @dir %* > %systemroot%\system32\ls.bat
+echo @ipconfig %* > %systemroot%\system32\ifconfig.bat
+echo @cls %* > %systemroot%\system32\clear.bat
+echo @chdir %* > %systemroot%\system32\pwd.bat
